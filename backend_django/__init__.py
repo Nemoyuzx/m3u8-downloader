c6e2b454cdcb348a4b7aaba5e38e4d3d0b3e907f
YZX_Studio (Nemoyuzx) <2099905168@qq.com>
2025-06-04 18:07:16 +0800

    Fix Celery initialization

--- backend_django/__init__.py
+++ backend_django/__init__.py
@@ -1,5 +1,11 @@
-# This will make sure the app is always imported when
-# Django starts so that shared_task will use this app.
-# from .celery import app as celery_app
+"""Initialize Celery when Django starts."""
 
-# __all__ = ('celery_app',)
+# This ensures the Celery app is loaded when Django
+# starts so that the ``shared_task`` decorator uses
+# our configured application. Without this import the
+# tasks fall back to the default Celery app which uses
+# ``pyamqp://guest@localhost//`` and results in connection
+# errors if RabbitMQ is not running.
+from .celery import app as celery_app
+
+__all__ = ("celery_app",)
